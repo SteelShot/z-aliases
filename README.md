d0466360ce9ae3cfc9c9c6e588ab16c4fa658da5
Džiugas Eiva <dziugaseiva@gmail.com>
2023-09-03 16:21:10 +0100

    Added 3 commands * ls -> [ogham/exa](https://github.com/ogham/exa) * find -> [sharkdp/fd](https://github.com/sharkdp/fd) * cat -> [sharkdp/bat](https://github.com/sharkdp/bat)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # z-aliases
 Z Shell (zsh) aliases for common linux command modern replacements
+
+### Supported programs
+* ls -> [ogham/exa](https://github.com/ogham/exa)
+* find -> [sharkdp/fd](https://github.com/sharkdp/fd)
+* cat -> [sharkdp/bat](https://github.com/sharkdp/bat)
---
+++ z-aliases.plugin.zsh
@@ -0,0 +1,40 @@
+#!/usr/bin/env zsh
+
+#
+# Copyright (C) 2023 Džiugas Eiva GPL-3.0-only
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+# exa aliases (https://github.com/ogham/exa)
+if (( ${+commands[exa]} )); then
+  alias ls='exa --group-directories-first'
+
+  alias lah='ls -lah'
+
+  if (( ${+commands[git]} )); then
+    alias lg='lah --git'
+  fi
+fi
+
+# fd aliases (https://github.com/sharkdp/fd)
+if (( ${+commands[fd]} )); then
+  alias find='fd --color always'
+fi
+
+# bat aliases (https://github.com/sharkdp/bat)
+if (( ${+commands[bat]} )); then
+  alias cat='bat'
+elif (( ${+commands[batcat]} )); then
+  alias cat='batcat'
+fi
